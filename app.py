677553f5ad651a5ab9289ef1f407b06dc33d5a5b
eai <alexander.enikeev@gmail.com>
2020-03-16 21:19:37 +0300

    Create all render_template functions. Add .gitignore and requirements.txt files

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.secret_key = 'Enikeev-project3-secret-phrase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///enikeev_project3.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/cart/')
+def render_cart():
+    return render_template('cart.html')
+
+@app.route('/account/')
+def render_account():
+    return render_template('account.html')
+
+@app.route('/login/')
+def render_login():
+    return render_template('login.html')
+
+@app.route('/register/')
+def render_register():
+    return render_template('register.html')
+
+@app.route('/logout/')
+def render_logout():
+    return render_template('auth.html')
+
+@app.route('/ordered/')
+def render_ordered():
+    return render_template('ordered.html')        
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Такой страницы нет'
+
+if __name__ == '__main__':
+    app.run()
